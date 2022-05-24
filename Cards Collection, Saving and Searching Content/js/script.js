a7956d1f2958337192cb8613de825a32c91c6b5a
Lisandra <lisandraferraz.s@hotmail.com>
2022-05-24 20:11:38 -0300

    adicionando menu hamburguer

--- Cards Collection, Saving and Searching Content/js/script.js
+++ Cards Collection, Saving and Searching Content/js/script.js
@@ -14,10 +14,22 @@ cardsGroup.forEach((item, i) => {
     let CardsGroupLarg = tmnCardsGroup.width;
 
     btnAft[i].addEventListener('click', () =>{
+        console.log('teste')
         item.scrollLeft += CardsGroupLarg;
-    })
+})
 
     btnBef[i].addEventListener('click', () =>{
+        console.log('teste2')
         item.scrollLeft -= CardsGroupLarg;
     })
-    })
+})
+
+/*script menu hamburguer */
+
+const menu_hamb = document.querySelector('.menu-hamb');
+const sidebar = document.querySelector('.sidebar');
+
+menu_hamb.addEventListener('click', () => {
+    menu_hamb.classList.toggle('is-active');
+    sidebar.classList.toggle('is-active');
+})
