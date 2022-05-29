dbd75c1f5ceee947ff4e0cb6bf6a64faedf927e7
Lisandra <lisandraferraz.s@hotmail.com>
2022-05-29 17:53:19 -0300

    adicionando api omdb e consulta funcional

--- Cards Collection, Saving and Searching Content/js/script.js
+++ Cards Collection, Saving and Searching Content/js/script.js
@@ -3,13 +3,6 @@ const cardsGroup = [...document.querySelectorAll('.cards-group')];
 const btnBef = [...document.querySelectorAll('.btnBef')];
 const btnAft = [...document.querySelectorAll('.btnAft')];
 
-
-/*
-btnBef.addEventListener('click', function(){
-    console.log('teste')
-})
-*/
-
 cardsGroup.forEach((item, i) => {
     let tmnCardsGroup = item.getBoundingClientRect();
     let CardsGroupLarg = tmnCardsGroup.width;
@@ -26,12 +19,9 @@ cardsGroup.forEach((item, i) => {
 })
 
 /*script menu hamburguer */
-
-
 var hambMenu = document.querySelector(".menu-toggle");
 var respHambMenu = document.querySelector(".menu-toggle-2");
 var exitToggle = document.querySelector(".exit-toggle");
-// var bodyC = [...document.querySelectorAll('body')];
 
 hambMenu.addEventListener('click', function() {
     console.log('teste');
@@ -47,3 +37,96 @@ exitToggle.addEventListener('click', function(){
     console.log('teste 3');
     document.querySelector("body").classList.remove("respActive");
 })
+
+// script p/ omdb api
+// title https://omdbapi.com/?s=avengers&page=1&apikey=60ee3e91
+// details http://www.omdbapi.com/?i=tt3896198&apikey=60ee3e91
+
+const searchBar = document.getElementById('search-bar');
+const searchList = document.getElementById('search-list');
+const cardsGroupFlex = document.getElementById ('cards-group');
+
+// loading content from omdb api
+async function loadContent(searchItem){
+    
+    const URL = `https://omdbapi.com/?s=${searchItem}&page=1&apikey=60ee3e91`;
+    const res = await fetch(`${URL}`);
+    const data = await res.json();
+
+    if(data.Response == "True") {
+        // console.log(data);
+        displayContentList(data.Search);
+    }
+}
+
+function findContent(){
+    let searchItem = (searchBar.value).trim();
+    console.log(searchItem);
+    if(searchItem.length > 0){
+        searchList.classList.remove('hide-search-list');
+        loadContent(searchItem);
+    }
+    else{
+        searchList.classList.add('hide-search-list');
+    }
+}
+
+function displayContentList(content){
+    searchList.innerHTML = "";
+
+    for(let idx = 0; idx < content.length; idx++){
+        let contentListItem = document.createElement('div');
+        contentListItem.dataset.id = content[idx].imdbID;
+        contentListItem.classList.add('search-list-item');
+        if(content[idx].Poster != "N/A")
+            moviePoster = content[idx].Poster;
+        else
+            moviePoster = "resources/img-not-found.png";
+        
+        contentListItem.innerHTML = `
+        <div class="search-item-poster">
+            <img src="${moviePoster}">
+        </div>
+        <div class="search-item-desc">
+            <h3>${content[idx].Title}</h3>
+            <p>${content[idx].Year}</p>
+        </div>
+        `;
+
+        searchList.appendChild(contentListItem);
+
+    }
+    loadContentDetails();
+}
+
+function loadContentDetails(){
+    const searchListContent = searchList.querySelectorAll('.search-list-item');
+    searchListContent.forEach(content => {
+        content.addEventListener('click', async () =>{
+            // console.log(content.dataset.id);
+            searchList.classList.add('hide-search-list');
+            searchBar.value = "";
+            const result = await fetch(`http://www.omdbapi.com/?i=${content.dataset.id}&apikey=60ee3e91`);
+            const contentDetails = await result.json();
+            console.log(contentDetails);
+            displayContentDetails(contentDetails);
+        });
+        // console.log(content);
+    });
+}
+
+function displayContentDetails(details){
+    cardsGroupFlex.innerHTML = `
+    <div class="card">
+        <img src="${(details.Poster != "N/A") ? details.Poster: "resources/img-not-found.png"}" alt="" class="card-img" />
+        <div class="card-description">
+            <p class="card-title">${details.Title}</p>
+            <p>
+            ${(details.Plot)}
+            </p>
+        </div>
+    </div>
+    `;
+}
+
+// loadContent('lord of the rings');
